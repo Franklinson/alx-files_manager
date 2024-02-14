9c363885519e1b0d689e56104ec588381a4b538d
Franklinson <franklinhassey@.com>
2024-02-14 23:49:20 +0000

    edit server file

--- server.js
+++ server.js
@@ -2,16 +2,13 @@
 // An express server
 
 const express = require('express');
-const router = require('./routes/index');
+const routes = require('./routes/index');
 
 const app = express();
-const PORT = process.env.PORT ? process.env.PORT : 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/', router);
+app.use('/', routes);
 
-app.listen(PORT, () => {
-  console.log(`The server is running on port: ${PORT}`);
-});
-
+app.listen(port);
 module.exports = app;
