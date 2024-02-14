dd313721e93f4f90c43f6491d12d5bbd213cc431
Franklinson <franklinhassey@.com>
2024-02-14 23:51:31 +0000

    revert edited server file

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 #!/usr/bin/node
 // An express server
 
-const express = require('express');
-const routes = require('./routes/index');
+import express from 'express';
+import router from './routes/index';
+
+const port = parseInt(process.env.PORT, 10) || 5000;
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/', routes);
+app.use('/', router);
 
-app.listen(port);
-module.exports = app;
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
+
+export default app;
